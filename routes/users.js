c1619bfb54a577f7ddea137cbe275571ee22d440
서현욱 <seo.hw@seohyeon-ug-ui-MacBookPro.local>
2022-02-06 21:38:00 +0900

    remove 삭제 불필요 코드 삭제

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const { UserImportBuilder } = require('firebase-admin/lib/auth/user-import-builder');
 const router = express.Router();
 const decodeIdToken = require("../middlewares/decodeIdToken");
 
-/* GET users listing. */
 router.post('/new', function(req, res, next) {
   const { name, uid, email } = req.currentUser;
 });
