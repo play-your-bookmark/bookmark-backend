8cd0e024d2da54a17f0137aacb8c95d6264f7ad7
서현욱 <seo.hw@seohyeon-ug-ui-MacBookPro.local>
2022-02-06 21:36:36 +0900

    Feat: 추가 firebase auth & decodeIdtoken 구현하기

---
+++ middlewares/decodeIdToken.js
@@ -0,0 +1,20 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("../firebaseKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+async function decodeIDToken(req, res, next) {
+  try {
+    const idToken = req.headers.authorization.split("Bearer ")[1];
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    
+    req.currentUser = decodedToken;
+    next();
+  } catch (err) {
+    next(createError(401));
+  }
+}
+
+module.exports = decodeIDToken;
