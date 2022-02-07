d8ea9fc06bc50a97b8b92653d3fa5b3e54678600
sihyunlee <39shlee@gmail.com>
2022-02-07 22:14:01 +0900

    feature: mongoDB 설정 및 Schema 추가

---
+++ models/Folder.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const folderSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  publisher: {
+    type: Object.Id,
+    ref: "User",
+  },
+  published_at: {
+    type: Date,
+  },
+  likes: [
+    {
+      type: Object.Id,
+      ref: "User",
+    },
+  ],
+  bookmark: [
+    {
+      type: Object.Id,
+      ref: "Bookmark",
+    },
+  ],
+  category: {
+    type: String,
+  },
+  parent_folder: {
+    type: Object.Id,
+    ref: "Folder",
+  },
+});
+
+module.exports = mongoose.model("Folder", folderSchema);
