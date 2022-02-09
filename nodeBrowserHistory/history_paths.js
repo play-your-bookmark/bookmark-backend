fb104a440ed5f6fd462370bb10138dba0fe7b241
hatae94 <gkxo5959@naver.com>
2022-02-09 22:44:38 +0900

    Refactor: 추가 win32 path 로직

--- nodeBrowserHistory/history_paths.js
+++ nodeBrowserHistory/history_paths.js
@@ -12,10 +12,23 @@ function setupForMac(defaultPaths) {
   );
 }
 
+function setupForWindows(defaultPaths) {
+  const appDataDirectory = Path.join(
+    process.env.HOMEDRIVE,
+    "Users",
+    process.env.USERNAME,
+    "AppData",
+  );
+
+  defaultPaths.chrome = Path.join(appDataDirectory, "Local", "Google", "Chrome");
+}
+
 function setupDefaultPaths(defaultPaths) {
   switch (process.platform) {
     case "darwin":
       return setupForMac(defaultPaths);
+    case "win32":
+      return setupForWindows(defaultPaths);
     default:
       console.error(`Platform ${process.platform} is not supported by node-browser-history`);
   }
