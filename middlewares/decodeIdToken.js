db8591ccf6ef55dc542445199736721b1bae54c1
서현욱 <hyunwookkotra@gmail.com>
2022-02-17 18:48:04 +0900

    Feat: 추가 firebase key 수정

--- middlewares/decodeIdToken.js
+++ middlewares/decodeIdToken.js
@@ -1,22 +1,21 @@
 const admin = require("firebase-admin");
 const createError = require("http-errors");
 
-// const serviceAccount = {
-//   type: process.env.FIRBASE_TYPE,
-//   project_id: process.env.PROJECT_ID,
-//   private_key_id: process.env.PRIVATE_KEY_ID,
-//   private_key: process.env.PRIVATE_KEY.replace(/\@/g, "\n"),
-//   client_email: process.env.CLIENT_EMAIL,
-//   client_id: process.env.CLIENT_ID,
-//   auth_uri: process.env.AUTH_URI,
-//   token_uri: process.env.TOKEN_URI,
-//   auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
-//   client_x509_cert_url: process.env.CLIENT_CERT_URL,
-// };
-const serviceAccount = process.env.MY_CREDENTIALS;
+const serviceAccount = {
+  type: process.env.FIRBASE_TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: `${process.env.PRIVATE_KEY}`,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_CERT_URL,
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(serviceAccount)),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 async function decodeIDToken(req, res, next) {
