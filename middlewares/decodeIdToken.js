b2669b8432465a3a35223ee11429a4cb86601ead
서현욱 <hyunwookkotra@gmail.com>
2022-02-17 16:23:54 +0900

    Fix: register firebase env

--- middlewares/decodeIdToken.js
+++ middlewares/decodeIdToken.js
@@ -1,7 +1,18 @@
 const admin = require("firebase-admin");
 const createError = require("http-errors");
 
-const serviceAccount = require("../firebaseKey.json");
+const serviceAccount = {
+  type: process.env.FIRBASE_TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
