1bd4bacf22aa8160f144ad90fe6162dddbc305ef
hatae94 <gkxo5959@naver.com>
2022-03-27 23:11:54 +0900

    REFACTOR: 수정 decodeIdToken.js

--- middlewares/decodeIdToken.js
+++ middlewares/decodeIdToken.js
@@ -1,9 +1,23 @@
 const admin = require("firebase-admin");
 const createError = require("http-errors");
-const serviceAccount = require("../firebaseKey.json");
+const AWS = require("aws-sdk");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+const s3 = new AWS.S3({
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECURE_ACCESS_KEY,
+});
+const params = { Bucket: "90crew-playyourbookmark2", Key: "firebaseKey.json" };
+
+s3.getObject(params, (err, data) => {
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    const body = JSON.parse(Buffer.from(data.Body).toString());
+
+    admin.initializeApp({
+      credential: admin.credential.cert(body),
+    });
+  }
 });
 
 async function decodeIDToken(req, res, next) {
